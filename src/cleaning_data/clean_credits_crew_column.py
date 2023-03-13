a0c8c52d3b7ec9ac4d3bb3c5e669f48d643d66fc
gabensaw <gabriel.sawicki1995@gmail.com>
2023-03-13 19:40:13 +0000

    clean by hand 3 people

--- src/cleaning_data/clean_credits_crew_column.py
+++ src/cleaning_data/clean_credits_crew_column.py
@@ -34,14 +34,27 @@ crew_film_id_df = pd.json_normalize(
     meta_prefix='film_'
 ).drop(['credit_id'], axis=1).drop_duplicates(ignore_index=True).reset_index(names='crew_id')
 
+
+############
+# CLEANING #
+############
+
+# clean wrong people -> check each time is all people valid
+crew_film_id_df.loc[crew_film_id_df.name == 'Sally Boldt', 'gender'] = 1
+crew_film_id_df.loc[crew_film_id_df.name == 'Peter Malota', 'gender'] = 2
+crew_film_id_df.loc[crew_film_id_df.name == 'Fruit Chan', 'profile_path'] = '/j1Xktbs6M6kNIwaLUps9swSEdVs.jpg'
+
+
+##################################
+# prepare df to match ERD tables #
+##################################
+
 # people_df before junction
 people_df = crew_film_id_df[['id', 'gender', 'name', 'profile_path']].drop_duplicates(ignore_index=True)
-#
-# 3 duplicates people below checking duplicates function
-# print(people_df[people_df[['id', 'name']].duplicated(keep=False)])
 
 # jobs df -> done
-jobs_before_merge = crew_film_id_df[['job', 'department']].drop_duplicates(ignore_index=True).reset_index(names='job_id')
+jobs_before_merge = crew_film_id_df[['job', 'department']].drop_duplicates(ignore_index=True).reset_index(
+    names='job_id')
 
 # departments df -> done
 departments_df = jobs_before_merge[['department']].drop_duplicates(ignore_index=True).reset_index(names='department_id')
@@ -59,4 +72,14 @@ crew_jobs_junction = make_junction_table(
 )
 
 # crew dataframe need to match with crew table from ERD
-#crew_df = crew_film_id_df
+# crew_df = crew_film_id_df
+# print(crew_film_id_df)
+
+
+####################
+# ERRORS TO REPAIR #
+####################
+
+# !!!!! here is an error job_name = Other, but this jobs have departments imputed -> what to do??
+# print(crew_film_id_df[crew_film_id_df['crew_id'] == 321438])
+# print(jobs_before_merge[jobs_before_merge['job'] == 'Other'])
