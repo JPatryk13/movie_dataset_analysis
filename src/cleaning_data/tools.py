a1747433d7c9f9dcd15cc7b2acdef2eb259daafd
jpatryk13 <jesionka.patryk13@gmail.com>
2023-03-25 18:41:35 +0000

    Edited to-do comment

--- src/cleaning_data/tools.py
+++ src/cleaning_data/tools.py
@@ -99,16 +99,15 @@ def make_junction_table(
     :param left_df:
     :param right_df:
     :param merge_on: list of column names to merge on in order: [<left_df_col_name>, <right_df_col_name>]
-    :param old_index_names: (optional) old index column names - by default both set to "index"
+    :param old_index_names: (optional) old index column names - by default both set to "index" (<left_df_index>,
+    <right_df_index>)
     :param new_index_names: (optional) renames index column names from index to [<left_df_fk>, <right_df_fk>]
     :return: junction table
     """
     # TODO:
     #  verify that <left_df_col_name>, <right_df_col_name> exist in left/right_df
     #  .
-    #  verify that both column names exist in left/right_df
-    #  .
-    #  verify that <left_df_fk>, <right_df_fk> exist in left/right_df
+    #  verify that both column names exist in left/right_df (<left_df_index>, <right_df_index>)
 
     left_df = left_df[[old_index_names[0], merge_on[0]]].rename(columns={old_index_names[0]: new_index_names[0]})
     right_df = right_df[[old_index_names[1], merge_on[1]]].rename(columns={old_index_names[1]: new_index_names[1]})
