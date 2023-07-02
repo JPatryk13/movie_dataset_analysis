c70185064d862d37d645e22f51b497cb961209a3
gabensaw <gabriel.sawicki1995@gmail.com>
2023-07-02 16:04:43 +0100

    converting timestamp column to datetime object

--- src/cleaning_data/clean_ratings_csv.py
+++ src/cleaning_data/clean_ratings_csv.py
@@ -9,3 +9,5 @@ archive_path = Path(__file__).resolve().parent.parent.parent / "dataset" / "arch
 ratings_df = pd.read_csv(archive_path / "ratings.csv", low_memory=False).drop_duplicates(ignore_index=True).rename(
     columns={'userId': 'user_id', 'movieId': 'movie_id'})
 
+# convert timestamp column to date time object representing date and time
+ratings_df['timestamp'] = pd.to_datetime(ratings_df['timestamp'], unit='s')
