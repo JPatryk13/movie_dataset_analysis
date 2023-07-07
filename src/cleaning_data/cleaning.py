a4d7e36f85619d32faf79f9b07128a500f8076e0
gabensaw <gabriel.sawicki1995@gmail.com>
2023-07-07 14:35:08 +0100

    start working on class CleanCast especially clean_cast_df method

--- src/cleaning_data/cleaning.py
+++ src/cleaning_data/cleaning.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import json
 from pathlib import Path
+from ast import literal_eval
 
 
 class CleanMoviesMetadata:
@@ -38,18 +40,57 @@ class CleanMoviesMetadata:
         return None
 
 
+class CleanCast:
+
+    def __init__(self):
+        archive_path = Path(__file__).resolve().parent.parent.parent / "dataset" / "archive"
+
+        self.df = pd.read_csv(archive_path / "credits.csv", low_memory=False).drop_duplicates(
+            ignore_index=True).rename(columns={'id': 'film_id'}).astype({'film_id': 'Int64'})[['film_id', 'cast']]
+
+    def clean_cast_df(self):
+        self.df['cast'] = self.df['cast'].apply(literal_eval)
+
+        df_to_json = self.df[['film_id', 'cast']].to_json(orient='records')
+
+        normalized_df = pd.json_normalize(
+            json.loads(df_to_json),
+            record_path='cast',
+            meta='film_id',
+        )
+
+        print(normalized_df)
+
+        return None
+
+    def get_cast_df(self):
+        # self.df = self.df.drop_duplicates(ignore_index=True)  # .set_index('film_id').sort_index()
+        return self.df
+
+
 #### napisać funkcję zwracającą czysty movies_df
 if __name__ == "__main__":
+    ### cleaning movies ###
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
 
-    cmm = CleanMoviesMetadata()
-    wrong_id_list = cmm.list_faulty_ids()
+    # cmm = CleanMoviesMetadata()
+    # wrong_id_list = cmm.list_faulty_ids()
+    #
+    # cmm.drop_unnecessary_columns(columns=['popularity', 'vote_average', 'vote_count'])
+    # cmm.drop_faulty_ids(wrong_ids=wrong_id_list)
+    # cmm.data_types_conversion()
+    #
+    # df = cmm.get_movies_df()
 
-    cmm.drop_unnecessary_columns(columns=['popularity', 'vote_average', 'vote_count'])
-    cmm.drop_faulty_ids(wrong_ids=wrong_id_list)
-    cmm.data_types_conversion()
+    # print(df)
 
-    df = cmm.get_movies_df()
+    ### cleaning cast ###
 
-    print(df)
+    cc = CleanCast()
+
+    cc.clean_cast_df()
+
+    cast_df = cc.get_cast_df()
+
+    # print(cast_df.dtypes)
